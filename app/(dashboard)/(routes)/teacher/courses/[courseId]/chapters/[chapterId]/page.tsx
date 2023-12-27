c63f8f49d6c7eb10e6a4654ac867f665134f40de
fkiptooh <fkiptooh@gmail.com>
2023-12-27 10:59:15 +0300

    feat(chapter) - ux/ui description using react-quill

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
   params,
@@ -67,6 +68,11 @@ const ChapterIdPage = async ({
           </div>
           {/* ChapterTitleForm */}
           <ChapterTitleForm
+            initialData={chapter}
+            chapterId={params.chapterId}
+            courseId={params.courseId}
+          />
+          <ChapterDescriptionForm
             initialData={chapter}
             chapterId={params.chapterId}
             courseId={params.courseId}
