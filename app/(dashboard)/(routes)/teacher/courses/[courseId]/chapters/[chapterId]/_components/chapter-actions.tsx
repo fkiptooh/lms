c2a66730cb084e5e363de6fc782384705d6ccd1f
fkiptooh <fkiptooh@gmail.com>
2023-12-31 18:02:36 +0300

    feat(api) - delete chapter, ux/ui

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+interface ChapterActionsProps {
+  disabled: boolean;
+  courseId: string;
+  chapterId: string;
+  isPublished: boolean;
+}
+export const ChapterActions = ({
+  disabled,
+  courseId,
+  chapterId,
+  isPublished,
+}: ChapterActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+      toast.success("Chapter deleted");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={() => {}}
+        variant="outline"
+        size="sm"
+        disabled={disabled || isLoading}
+      >
+        {!isPublished ? "Publish" : "Unpublish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" variant="destructive" disabled={isLoading}>
+          <Trash />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
