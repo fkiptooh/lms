fc315e6cef429582267882dfa6aa34d00636e1b6
fkiptooh <fkiptooh@gmail.com>
2024-01-02 02:38:29 +0300

    feat(ux/ui) - confetti

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/actions.tsx
@@ -2,6 +2,7 @@
 
 import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
+import { useConffettiStore } from "@/hooks/use-confetti-store";
 import axios from "axios";
 import { Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -16,6 +17,7 @@ interface ActionsProps {
 export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+  const confetti = useConffettiStore();
 
   const onClick = async () => {
     try {
@@ -26,6 +28,7 @@ export const Actions = ({ disabled, courseId, isPublished }: ActionsProps) => {
       } else {
         await axios.patch(`/api/courses/${courseId}/publish`);
         toast.success("Course published!");
+        confetti.onOpen();
       }
       router.refresh();
     } catch {
