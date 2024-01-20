6f245a025f346c6dd6eada0221729f649d0e534b
fkiptooh <fkiptooh@gmail.com>
2024-01-20 11:34:22 +0300

    (feat) - course listing

--- components/course-list.tsx
+++ components/course-list.tsx
@@ -1,4 +1,5 @@
 import { Category, Course } from "@prisma/client";
+import { CourseCard } from "./course-card";
 
 type CourseWithProgressWithCategory = Course & {
   category: Category | null;
@@ -13,9 +14,18 @@ interface CourseListProps {
 export const CoursesList = ({ items }: CourseListProps) => {
   return (
     <div>
-      <div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {items.map((item) => (
-          <div key={item.id}>{item.title}</div>
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl!}
+            chaptersLength={item.chapters.length}
+            price={item.price!}
+            progress={item.progress}
+            category={item?.category?.name!}
+          />
         ))}
       </div>
       {items.length === 0 && (
