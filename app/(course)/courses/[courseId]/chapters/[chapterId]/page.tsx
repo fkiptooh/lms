1af4c493e0f8dd430a7df0c5d90d5a60a20c4249
fkiptooh <fkiptooh@gmail.com>
2024-02-02 13:44:49 +0300

    feat(actions) - getChapters

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,16 @@
-const ChapterIdPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (userId) {
+    return redirect(`/`);
+  }
   return <div>Chapter Id</div>;
 };
 
