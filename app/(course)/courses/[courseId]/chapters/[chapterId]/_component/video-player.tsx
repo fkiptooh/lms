e6b87bebdf4fa78f83a7322ec637265f547654de
Felix Kiptoo <fkiptooh@gmail.com>
2024-02-06 12:19:24 +0300

    feat(player) - chapter

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_component/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_component/video-player.tsx
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import MuxPlayer from "@mux/mux-player-react";
-import {  useState } from "react";
+import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Loader2, Lock } from "lucide-react";
@@ -10,39 +10,49 @@ import { cn } from "@/lib/utils";
 import { useConffettiStore } from "@/hooks/use-confetti-store";
 
 interface IVideoPlayer {
-    playbackId: string;
-    courseId: string;
-    chapterId: string;
-    nextChapterId?: string;
-    isLocked: boolean;
-    completeOnEnd: boolean;
-    title: string;
+  playbackId: string;
+  courseId: string;
+  chapterId: string;
+  nextChapterId?: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+  title: string;
 }
 
-export const VideoPlayer= ( {
-    playbackId,
-    courseId,
-    chapterId,
-    nextChapterId,
-    isLocked,
-    completeOnEnd,
-    title
+export const VideoPlayer = ({
+  playbackId,
+  courseId,
+  chapterId,
+  nextChapterId,
+  isLocked,
+  completeOnEnd,
+  title,
 }: IVideoPlayer) => {
-    return(
-        <div className="relative aspect-video">
-            {!isLocked && (
-                <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
-                    <Loader2 className="h-8 w-8 animate-spin text-secondary"/>
-                </div>
-            )}
-            {isLocked && (
-                <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
-                    <Lock className="h-8 w-8"/>
-                    <p className="text-sm">
-                        This Chapter is Locked
-                    </p>
-                </div>
-            )}
+  const [isReady, setIsReady] = useState(false);
+
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
         </div>
-    )
-}
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">This Chapter is Locked</p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  );
+};
